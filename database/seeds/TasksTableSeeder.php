3eaf6b35ed2e404fbd5cd9c2f9a327fc316f1a60
Jorge Guimaraes <jorginhoad@gmail.com>
2018-11-30 19:01:49 -0200

    Criacao da seed TaskTableSeeder

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dados = [
+          [
+              "titulo" => "Levar o filho a escola",
+              "descricao" => "As 7:45 AM levar o filho a escola todas as manhã",
+              "status" => "1",
+              "data" =>"2018/01/01",
+              "conclusao" =>false
+          ],
+        ];
+
+        $tasks = new \App\Tasks();
+        foreach ($dados as $dado)
+        {
+            $tasks->fill($dado)->save();
+        }
+
+
+
+    }
+}
