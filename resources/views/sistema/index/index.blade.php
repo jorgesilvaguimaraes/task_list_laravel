35a60933e4e759436f473357547a8baf00e61e34
Jorge Guimaraes <jorginhoad@gmail.com>
2018-12-01 17:55:50 -0200

    criando layout para aplicacao Tasklist

---
+++ resources/views/sistema/index/index.blade.php
@@ -0,0 +1,150 @@
+@extends('layouts.sistema')
+    @section('conteudo')
+
+        <div class="row">
+            <div class="col-sm-12">
+                <button class="btn btn-primary btn-lg btn-block">Nova Tarefa</button>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                            </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal text-center" >
+                            <span class="badge badge-primary col-sm-12">Andamento - 03/01/18</span>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pricing-card-title" data-toggle="tooltip" data-placement="top" title="For proper cross-browser and cross-platform behavior, you must use the <a> tag, not the <button> tag, and you also must include a tabindex attribute.">Levar o levi para escola as 7:30 pm </h5>
+                        <p class="text-justify">
+                        </p>
+                        <button type="button" class="btn btn-sm btn-primary">Editar</button>
+                        <button type="button" class="btn btn-sm btn-success">Concluir</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    @stop
