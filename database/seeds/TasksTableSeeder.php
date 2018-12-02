b5a7ffaea7ac552ba38670702f08d335eda12db7
Jorge Guimaraes <jorginhoad@gmail.com>
2018-12-02 02:32:02 -0200

    alteracao non arquivo TasksTableSeeder

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -12,20 +12,37 @@ class TasksTableSeeder extends Seeder
     public function run()
     {
         $dados = [
-          [
-              "titulo" => "Levar o filho a escola",
-              "descricao" => "As 7:45 AM levar o filho a escola todas as manhã",
-              "status_id" => 1,
-              "data" =>"2018/01/01",
-          ],
+            [
+                "titulo" => "Leva filho a escola",
+                "descricao" => "As 7:45 AM levar o filho a escola todas as manhã",
+                "status_id" => 1,
+                "data" => "2018/01/01",
+            ],
+            [
+                "titulo" => "Consulta com Gastro",
+                "descricao" => "Ir para consulta com o gastro as 10:00 pm",
+                "status_id" => 2,
+                "data" => "2018/01/01",
+            ],
+            [
+                "titulo" => "Ir buscar o filho na escola",
+                "descricao" => "As 11:00 pm ir buscar o filho na escola e deixar na aula de natação",
+                "status_id" => 3,
+                "data" => "2018/01/01",
+            ],
+            [
+                "titulo" => "Ir buscar o filho na escola de natação",
+                "descricao" => "Não esquecer de buscar o filho na aula de natação e deixar em casa as 12:00",
+                "status_id" => 4,
+                "data" => "2018/01/01",
+            ]
+
         ];
 
-        $tasks = new \App\Entities\Tasks();
-        foreach ($dados as $dado)
-        {
+        foreach ($dados as $dado) {
+            $tasks = new \App\Entities\Tasks();
             $tasks->fill($dado)->save();
         }
-
 
 
     }
